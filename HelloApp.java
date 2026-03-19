57874ddb139340a6e833780fa3b91c4e25e27051
Dhruti-007 <pp2909@srmist.edu.in>
2026-03-19 22:11:44 +0530

    Added UC1

---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+/**
+ * HelloApp.java - A simple Java application that Displays "Hello, World!"
+ * to the console.
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+        // This prints the message to the console
+        System.out.println("Hello, World!");
+    }
+}
